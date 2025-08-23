d020425bc5fe604b02d83f5328721bd82e4b3214
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-08-23 13:21:44 -0300

    Update _validCommand.js

--- plugins/_validCommand.js
+++ plugins/_validCommand.js
@@ -19,7 +19,7 @@ if (validCommand(command, global.plugins)) {
 let chat = global.db.data.chats[m.chat]
 let user = global.db.data.users[m.sender]    
 if (chat.isBanned) {
-const avisoDesactivado = `《🌙》El bot *${botname}* está desactivado en este grupo.\n\n> ⚠️ Un *administrador* puede activarlo con el comando:\n> » *${usedPrefix}bot on*`
+const avisoDesactivado = `> ✧ El bot *${botname}* está desactivado en este grupo.\n\n> ⚠️ Un *administrador* puede activarlo con el comando:\n> » *${usedPrefix}bot on*`
 await m.reply(avisoDesactivado)
 return
 }    
@@ -29,5 +29,5 @@ user.commands = 0
 user.commands += 1
 } else {
 const comando = m.text.trim().split(' ')[0]
-await m.reply(`《🌙》El comando *${comando}* no existe.\nPara ver la lista de comandos usa:\n» *#help*`)
+await m.reply(`╭━⊱ ❌ 𝗘𝗦𝗧𝗘 𝗖𝗢𝗠𝗔𝗡𝗗𝗢 𝗡𝗢 𝗘𝗫𝗜𝗦𝗧𝗘 ❌ ⊱━╮\n┃ 💬 𝗛𝗼𝗹𝗮 𝗲𝗹 𝗰𝗼𝗺𝗮𝗻𝗱𝗼 *${comando}* 𝗻𝗼 𝗲𝘅𝗶𝘀𝘁𝗲.\n┃ 📌 𝗨𝘀𝗮: *#help*\n┃ 🧾 𝗣𝗮𝗿𝗮 𝘃𝗲𝗿 𝗹𝗼𝘀 𝗰𝗼𝗺𝗮𝗻𝗱𝗼𝘀 𝗱𝗶𝘀𝗽𝗼𝗻𝗶𝗯𝗹𝗲𝘀\n╰━━━━━━━━━━━━━━━━━╯`)
 }}
